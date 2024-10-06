da512e20b884f3e4744ce3cfa44b601433e41ee1
Damir37 <damir.mullakhmetov.02@bk.ru>
2024-10-06 10:17:28 +0700

    FIX: название роутов
    Обращай внимавние на то, как ты наызваешь роуты и как они назваты в кликапе
    Например:
    vacancyGroupV1.GET("/vacancies/:id", vacancyHandler.GetVacancyByID)
    По роуту можно понять, что вроде ед. вакансию получить по айди, а вроде и множественное число

--- internal/api/http/v1/routs.go
+++ internal/api/http/v1/routs.go
@@ -4,15 +4,16 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(router *gin.Engine, vacancyHandler *vacancyHandler) {
 	router.Use(metricsMiddleware())
+
 	vacancyGroupV1 := router.Group("/v1")
 	{
 		vacancyGroupV1.POST("/create-vacancy", vacancyHandler.CreateVacancy)
 
 		vacancyGroupV1.GET("/vacancies", vacancyHandler.GetAllVacancies)
-		vacancyGroupV1.GET("/vacancies/:id", vacancyHandler.GetVacancyByID)
+		vacancyGroupV1.GET("/vacancy/:id", vacancyHandler.GetVacancyByID)
 
-		vacancyGroupV1.PATCH("/vacancies/:id", vacancyHandler.EditVacancy)
+		vacancyGroupV1.PATCH("/vacancy/:id", vacancyHandler.EditVacancy)
 
-		vacancyGroupV1.DELETE("/vacancies/:id", vacancyHandler.DeleteVacancy)
+		vacancyGroupV1.DELETE("/vacancy/:id", vacancyHandler.DeleteVacancy)
 	}
 }
